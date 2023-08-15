81b3a490284016cbe63e5db7a609029e3023e713
Keegan <juan789huarcaya@gmail.com>
2023-08-15 00:20:25 -0500

    agregando swagger

--- src/main/java/com/cinema/CinemaApplication.java
+++ src/main/java/com/cinema/CinemaApplication.java
@@ -3,11 +3,21 @@ package com.cinema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CinemaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
 	}
 
+	public Docket apis() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.cinema")).build();
+	}
+	
 }
